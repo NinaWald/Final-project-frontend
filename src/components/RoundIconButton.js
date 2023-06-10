537a84a85db1f46bc6edcb13d86632e7aaf7450a
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-06-11 01:29:38 +0200

    added display inline to roundiconbutton

--- src/components/RoundIconButton.js
+++ src/components/RoundIconButton.js
@@ -1,36 +1,28 @@
 import React from 'react';
-import { styled } from '@mui/system';
-import { Button } from '@mui/material';
+import styled from 'styled-components';
+import { Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 
-const StyledButton = styled(Button)`
-  background-color: #669999;
-  color: #fff;
-  border-radius: 50%;
-  padding: 4px;
-  width: 22px;
-  height: 62px;
-  display: flex;
-  align-items: center;
+const ButtonContainer = styled.div`
   justify-content: center;
-  display: inline-flex;
-  gap: 4px;
-  margin: 0;
-
-  &:hover {
-    background-color: #DDDDBB;
-  }
+  display: inline;
+  margin: 5px;
 `;
 
-const RoundIconButton = ({ children, variant, color, disabled, onClick, isRemove }) => {
+const RoundIconButton = ({ color, disabled, onClick, isRemove }) => {
   const icon = isRemove ? <RemoveIcon /> : <AddIcon />;
 
   return (
-    <StyledButton variant={variant} color={color} disabled={disabled} onClick={onClick}>
-      {icon}
-      {children}
-    </StyledButton>
+    <ButtonContainer>
+      <Fab
+        size="small"
+        color={color}
+        disabled={disabled}
+        onClick={onClick}>
+        {icon}
+      </Fab>
+    </ButtonContainer>
   );
 };
 
